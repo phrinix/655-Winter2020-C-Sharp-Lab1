efa6b4613f413668db14570e953672562ed3ba31
phrinix <60997906+phrinix@users.noreply.github.com>
2020-06-22 04:49:32 -0400

    Add files via upload

---
+++ Lab1/Lab1/bar_form.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Lab1
+{
+    partial class bar_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.CTech_data = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MS_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.CTech_data)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CTech_data
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.CTech_data.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.CTech_data.Legends.Add(legend1);
+            this.CTech_data.Location = new System.Drawing.Point(12, 46);
+            this.CTech_data.Name = "CTech_data";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Average Call Time(mins)";
+            this.CTech_data.Series.Add(series1);
+            this.CTech_data.Size = new System.Drawing.Size(905, 481);
+            this.CTech_data.TabIndex = 0;
+            this.CTech_data.Text = "chart1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(929, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MS_Exit});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // MS_Exit
+            // 
+            this.MS_Exit.Name = "MS_Exit";
+            this.MS_Exit.Size = new System.Drawing.Size(116, 26);
+            this.MS_Exit.Text = "Exit";
+            this.MS_Exit.Click += new System.EventHandler(this.MS_Exit_Click);
+            // 
+            // bar_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 539);
+            this.Controls.Add(this.CTech_data);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "bar_form";
+            this.Text = "Technician data in graphical form";
+            ((System.ComponentModel.ISupportInitialize)(this.CTech_data)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart CTech_data;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MS_Exit;
+    }
+}
---
+++ Lab1/Lab1/bar_form.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class bar_form : Form
+    {
+        Technician[] arr;
+        int x;
+        public bar_form(int _x, Technician[] _arr)
+        {
+            InitializeComponent();
+            arr = _arr;
+            x = _x;
+            chart_show_data();
+        }
+
+        private void chart_show_data()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                CTech_data.Series[0].Points.AddXY(arr[i].Tech_name, arr[i].Time_average);
+            }
+            CTech_data.Series[0].IsValueShownAsLabel = true;
+            CTech_data.Titles.Add("Technician Average Call Time Data");
+            CTech_data.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+        }
+
+        private void MS_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
